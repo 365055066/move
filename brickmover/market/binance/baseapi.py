e71aa34f59a41fca956e9edf4f5029ab781663d0
365055066 <365055066@qq.com>
2018-04-26 11:10:24 +0800

    update code

--- brickmover/market/binance/baseapi.py
+++ brickmover/market/binance/baseapi.py
@@ -1,2 +1,40 @@
+from brickmover.market.marketbase  import MarketBase
+import brickmover.market.binance.rest.api as restapi
 
+class BaseApi(MarketBase):
+    def __init__(self,key='',secret='',exchange='',target='',base='',price_min_move=100000000,order_size_min=100000000):
+        super(BaseApi, self).__init__(exchange,target,base,price_min_move,order_size_min)  
+        self.restapi = restapi.Api(key,secret)
+        self.symbol = target.upper() + base.upper()
+        
+    def GetTicker(self):
+        response =self.restapi.get_symbol_ticker(symbol=self.symbol)
+        print(response)
+        pass
+    
+    def GetDepth(self):
+        pass    
 
+    def GetTrades(self):
+        pass     
+
+    #trade
+    def Login(self):
+        pass
+    
+    def Buy(self,price,quantitye):
+        pass
+ 
+    def Sell(self,price,quantity):
+        pass
+   
+    def CancelOrder(self,orderid=None):
+        pass
+    
+    def GetOrders(self,orderid=None):
+        pass
+    
+    def GetAccountInfo(self):
+        pass    
+
+    
--- brickmover/market/marketbase.py
+++ brickmover/market/marketbase.py
@@ -1,105 +1,55 @@
 
-from brickmover.eventengie import Event
-from brickmover.eventengie import EVENT_TYPE
-from brickmover.eventengie import EVENT_DATA
-from brickmover.eventengie import EVENT_LOGIN
-from brickmover.eventengie import EVENT_DEPTH
-from brickmover.eventengie import EVENT_TICK
-from brickmover.eventengie import EVENT_ORDER
-from brickmover.eventengie import EVENT_ACCOUNTINFO
 
 class MarketBase(object):
-    def __init__(self,exchange,symbol,currency,price_min_move,order_size_min,eventengie):
+    def __init__(self,exchange,target,base,price_min_move=100000000,order_size_min=100000000):
         self.exchange = exchange
-        self.symbol = symbol
-        self.currency = currency
-        self.marketcode = self.exchange+ self.symbol +self.currency
+        self.target = target
+        self.base = base
+        self.marketcode = self.exchange+ self.target +self.base
         
-        self.eventengie = eventengie
-
         self.price_min_move = price_min_move
         self.price_precision = 1/self.price_min_move
         self.order_size_min = order_size_min
         self.order_size_precision = 1/self.order_size_min
         
-        self.symbol_reserved = 0
-        self.symbol_available = 0
-        self.currency_reserved = 0
-        self.currency_available = 0
+        #self.symbol_reserved = 0
+        #self.symbol_available = 0
+        #self.currency_reserved = 0
+        #self.currency_available = 0
+   
+    def GetMarketCode(self):
+        return self.marketcode   
    
     #market info
-    def GetTicker(self,isasync=False):
+    def GetTicker(self):
         pass
     
-    def GetDepth(self,isasync=False):
+    def GetDepth(self):
         pass    
 
-    def GetTrades(self,isasync=False):
+    def GetTrades(self):
         pass     
-    
-    def GetMarketCode(self):
-        return self.marketcode
-    
+
     #trade
-    def Login(self,async=False):
+    def Login(self):
         pass
     
-    def Buy(self,price,quantity,isasync=False):
+    def Buy(self,price,quantitye):
         pass
  
-    def Sell(self,price,quantity,isasync=False):
+    def Sell(self,price,quantity):
         pass
    
-    def CancelOrder(self,orderid=None,isasync=False):
+    def CancelOrder(self,orderid=None):
         pass
     
-    def GetOrders(self,orderid=None,isasync=False):
+    def GetOrders(self,orderid=None):
         pass
     
-    def GetAccountInfo(self,isasync=True):
+    def GetAccountInfo(self):
         pass    
-    
-    def SubscribeMarketInfo(self):
-        pass
-    
-    def SubscribeTradeInfo(self):
-        pass
-        
-    
-    def _OnLogined(self,status):
-        ev = Event(self.marketcode,{EVENT_TYPE:EVENT_LOGIN,
-                                    EVENT_DATA:status})
-        self.eventengie.putEvent(ev)        
-    
-    def _OnTick(self,tick):
-        ev = Event(self.marketcode,{EVENT_TYPE:EVENT_TICK,
-                                    EVENT_DATA:tick})
-        self.eventengie.putEvent(ev)
-    
-    def _OnDepth(self,depth):
-        ev = Event(self.marketcode,{EVENT_TYPE:EVENT_DEPTH,
-                                    EVENT_DATA:depth})
-        self.eventengie.putEvent(ev)
-    
-    #trade info stream
-    def _OnOrder(self,order):
-        '''
-        status: new, suspended, partiallyFilled, filled, canceled, expired
-        side:  sell or buy
-        quantity: Order quantity
-        price: Order price
-        quantityExcuted: Cumulative executed quantity
-        orderId: Unique identifier for Order as assigned by exchange
-        clientOrderId: Unique identifier for Order as assigned by trader.
-        updatetime: Datetime
-        '''
-        ev = Event(self.marketcode,{EVENT_TYPE:EVENT_ORDER,
-                                    EVENT_DATA:order})
-        self.eventengie.putEvent(ev)
-  
-    def _OnAccountInfo(self,accountinfo):
-        ev = Event(self.marketcode,{EVENT_TYPE:EVENT_ACCOUNTINFO,
-                                    EVENT_DATA:accountinfo})
-        self.eventengie.putEvent(ev) 
+
+
+
 
     
